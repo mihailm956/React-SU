a57c306f74ab65bc0eeebf7d440b551ef8843d56
mihailm956 <mbrave2627@gmail.com>
2020-08-14 14:24:04 +0300

    feat: navigation + header

--- issue-terminal/src/navigation.js
+++ issue-terminal/src/navigation.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
     BrowserRouter,
     Switch,
     Route
 } from 'react-router-dom';
 
-import HomePage from './pages/home/homePage';
+import HomePage from './pages/home/HomePage';
 
-const Navigation = (props) => {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={HomePage} />
-            </Switch>
-        </BrowserRouter>
-    );
-};
+class Navigation extends Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState({ showSideDrawer: !this.state.showSideDrawer });
+    }
+
+    render() {
+        
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact component={HomePage} />
+                </Switch>
+            </BrowserRouter>
+        );
+    }
+}
+
 
 export default Navigation;
--- issue-terminal/src/pages/home/homePage.js
+++ issue-terminal/src/pages/home/homePage.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 
+import Header from '../../components/navigation/header/Header';
+import style from './home-page.module.css';
+
 class homePage extends Component {
     render() {
         return (
             <div>
-                <p>HOME PAGE</p>
+                {/* {this.props.children} */}
+                <Header />
+                <main className={style.Container}>
+                    HomePage
+                </main>
             </div>
         )
     }
