513137233a020a7adaa7c653c424c6721a4f00ad
mihailm956 <mbrave2627@gmail.com>
2020-08-23 11:10:50 +0300

    feat: homepage information

--- issue-terminal/src/pages/home/homePage.js
+++ issue-terminal/src/pages/home/homePage.js
@@ -2,12 +2,40 @@ import React, { Component } from 'react'
 
 import style from './home-page.module.css';
 import PageLayout from '../../components/pageLayout/PageLayout';
+import imgLogo from '../../assets/images/Logo.png';
+import IconAccess from '../../assets/images/IconAccess.png';
+import IconDate from '../../assets/images/IconDate.png';
+import IconEdit from '../../assets/images/IconEdit.png';
+import IconNewPage from '../../assets/images/IconNewPage.png';
 
 class HomePage extends Component {
     render() {
         return (
             <PageLayout>
-                <div>Home Page</div>
+                <main>
+                    <div className={style.Container}>
+                        <img src={imgLogo} alt="Issue-Terminal" />
+                        BugTracer is an issue tracker that provides a delicate balance between simplicity and power.
+                        <br/>
+                        <br/>
+                        <br/>
+                        <div></div>
+                        <div className={style.MultipleContainers}>
+                            <div className={style.Advantages}>
+                                <img src={IconAccess} alt="Issue-Terminal" />
+                                role based access
+                            </div>
+                            <div className={style.Advantages}>
+                                <img src={IconDate} alt="Issue-Terminal" />
+                                Notify if bug is dueDate
+                            </div>
+                            <div className={style.Advantages}>
+                                <img src={IconEdit} alt="Issue-Terminal" />
+                                Easly edit ongoing bugs
+                            </div>
+                        </div>
+                    </div>
+                </main>
             </PageLayout>
         )
     }
